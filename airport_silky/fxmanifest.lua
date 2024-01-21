2932214795554a959cb21eb9b64b67bd6fbd840c
Tibo-MLO <135160241+Tibo-MLO@users.noreply.github.com>
2024-01-21 22:00:34 +0100

    test_release

---
+++ airport_silky/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+game 'gta5'
+this_is_a_map 'yes'
+replace_level_meta 'gta5'
+
+file{
+    'gta5.meta',
+    'doortuning.ymt'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/garage/**.ytyp'
